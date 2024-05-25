d4570e793937d62399f0546a5f5fc781e5cef146
Dont Knew <israfil123.sa@gmail.com>
2024-05-25 12:30:23 +0530

    update

--- complete/back-end/login-with-social/google/config.php
+++ complete/back-end/login-with-social/google/config.php
@@ -8,7 +8,6 @@ define('GOOGLE_REDIRECT_URL', 'http://localhost/PHP/authgoogle/');
 //Include Google Client Library for PHP autoload file
 require_once 'vendor/autoload.php';
 
-//Make object of Google API Client for call Google API
 $google_client = new Google_Client();
 
 //Set the OAuth 2.0 Client ID
@@ -23,6 +22,5 @@ $google_client->setRedirectUri(GOOGLE_REDIRECT_URL);
 $google_client->addScope('email');
 $google_client->addScope('profile');
 
-//start session on web page
 session_start();
 ?>
